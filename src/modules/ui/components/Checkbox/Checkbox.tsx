09ecf209be5ea7940c2419ac77b1fb95331aa7f4
adipol1359 <aditv1337@gmail.com>
2023-07-16 13:22:09 +0200

    feat(ui): add rhf props

--- src/modules/ui/components/Checkbox/Checkbox.tsx
+++ src/modules/ui/components/Checkbox/Checkbox.tsx
@@ -1,23 +1,35 @@
-import { forwardRef, type ReactNode } from 'react';
+import { forwardRef } from 'react';
 import { twMerge } from 'tailwind-merge';
 
 // TODO: FIX STORYBOOK
 import Checkmark from '../../../../assets/svg/checkmark.svg';
 
+import type { ChangeEventHandler, FocusEventHandler, ReactNode } from 'react';
+
 type CheckboxProps = Readonly<{
 	muted?: boolean;
+	name?: string;
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	onBlur?: FocusEventHandler<HTMLInputElement>;
 	label: ReactNode;
 }>;
 
 export const Checkbox = forwardRef<HTMLInputElement, CheckboxProps>(
-	({ muted, label }, ref) => (
+	({ muted, name, onChange, onBlur, label }, ref) => (
 		<label
 			className={twMerge(
 				'flex w-fit cursor-pointer items-center gap-x-2 text-xs font-medium text-text-normal',
 				muted && 'text-text-muted',
 			)}
 		>
-			<input ref={ref} type="checkbox" className="peer hidden" />
+			<input
+				ref={ref}
+				name={name}
+				onChange={onChange}
+				onBlur={onBlur}
+				type="checkbox"
+				className="peer hidden"
+			/>
 			<div className="flex h-6 w-6 shrink-0 items-center justify-center rounded-md border border-primary-400 peer-checked:border-brand-experiment-400 peer-checked:bg-brand-experiment-500 [&>*]:opacity-0 peer-checked:[&>*]:opacity-100">
 				<Checkmark aria-hidden="true" />
 			</div>
