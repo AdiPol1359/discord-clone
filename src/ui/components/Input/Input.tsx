55149126e8fcd1fd9ad29d332d9d2c5f5f68df33
adipol1359 <aditv1337@gmail.com>
2023-07-21 13:00:18 +0200

    feat(ui): add missing props

--- src/ui/components/Input/Input.tsx
+++ src/ui/components/Input/Input.tsx
@@ -1,20 +1,33 @@
 import { forwardRef } from 'react';
 import { twJoin } from 'tailwind-merge';
 
-import type { ChangeEventHandler, FocusEventHandler, ReactNode } from 'react';
+import type {
+	AriaRole,
+	ChangeEventHandler,
+	CompositionEventHandler,
+	FocusEventHandler,
+	KeyboardEventHandler,
+	ReactNode,
+} from 'react';
 
 type InputProps = Readonly<{
 	id?: string;
 	name?: string;
+	role?: AriaRole;
 	type?: 'text' | 'email' | 'password';
 	label?: string;
 	placeholder?: string;
+	defaultValue?: string;
 	autoComplete?: boolean;
 	spellCheck?: boolean;
+	disabled?: boolean;
 	required?: boolean;
 	icon?: ReactNode;
-	onChange?: ChangeEventHandler<HTMLInputElement>;
 	onBlur?: FocusEventHandler<HTMLInputElement>;
+	onChange?: ChangeEventHandler<HTMLInputElement>;
+	onKeyDown?: KeyboardEventHandler<HTMLInputElement>;
+	onCompositionStart?: CompositionEventHandler<HTMLInputElement>;
+	onCompositionEnd?: CompositionEventHandler<HTMLInputElement>;
 }>;
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
@@ -22,15 +35,22 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
 		{
 			id,
 			name,
+			role,
 			type = 'text',
 			label,
 			placeholder,
+			defaultValue,
 			autoComplete = true,
 			spellCheck = true,
+			disabled,
 			required,
 			icon,
-			onChange,
 			onBlur,
+			onChange,
+			onKeyDown,
+			onCompositionStart,
+			onCompositionEnd,
+			...props
 		},
 		ref,
 	) => (
@@ -39,18 +59,25 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
 				ref={ref}
 				id={id}
 				name={name}
+				role={role}
 				type={type}
 				aria-label={label}
 				placeholder={placeholder}
+				defaultValue={defaultValue}
+				disabled={disabled}
 				required={required}
-				onChange={onChange}
 				onBlur={onBlur}
+				onChange={onChange}
+				onKeyDown={onKeyDown}
+				onCompositionStart={onCompositionStart}
+				onCompositionEnd={onCompositionEnd}
 				className={twJoin(
 					'w-full rounded bg-input-background p-2.5 text-text-normal placeholder-text-muted focus:outline-0',
 					icon && 'pr-9',
 				)}
 				{...(!autoComplete && { autoComplete: 'off' })}
 				{...(!spellCheck && { spellCheck })}
+				{...props}
 			/>
 			{icon && (
 				<div className="pointer-events-none absolute right-3 top-1/2 -translate-y-1/2 [&>*]:fill-interactive-normal">
