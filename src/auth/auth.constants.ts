bb3bf8fbe3e56284f42cb22cc849ddebf1274080
Adrian Polak <aditv1337@gmail.com>
2023-07-26 09:32:35 +0200

    feat(auth): add max password length in form validation (#20)

--- src/auth/auth.constants.ts
+++ src/auth/auth.constants.ts
@@ -1,8 +1,10 @@
 export const USERNAME_MIN_LENGTH = 2;
 export const USERNAME_MAX_LENGTH = 32;
 export const PASSWORD_MIN_LENGTH = 8;
+export const PASSWORD_MAX_LENGTH = 72;
 
 export const EMPTY_FIELD_ERROR_MESSAGE = 'This field is required';
 export const EMAIL_ERROR_MESSAGE = 'Not a well formed email address';
 export const USERNAME_ERROR_MESSAGE = `Must be between ${USERNAME_MIN_LENGTH} and ${USERNAME_MAX_LENGTH} in length`;
-export const PASSWORD_ERROR_MESSAGE = `Must be at least ${PASSWORD_MIN_LENGTH} characters long`;
+export const PASSWORD_MIN_LENGTH_ERROR_MESSAGE = `Must be at least ${PASSWORD_MIN_LENGTH} characters long`;
+export const PASSWORD_MAX_LENGTH_ERROR_MESSAGE = `Must be ${PASSWORD_MAX_LENGTH} or fewer in length`;
