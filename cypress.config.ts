c9de23ba3c89249bd4206db16f5b769001fbac9a
adipol1359 <aditv1337@gmail.com>
2023-07-27 12:21:45 +0200

    feat: add base url to the cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,8 +2,6 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
 	e2e: {
-		setupNodeEvents() {
-			// implement node event listeners here
-		},
+		baseUrl: 'http://localhost:3000',
 	},
 });
