3cbff772a0c9843147e85dc7cdbf96e5b48e3ba5
adipol1359 <aditv1337@gmail.com>
2023-07-31 12:05:32 +0200

    feat: add unit tests to auth forms

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,9 +1,10 @@
 import react from '@vitejs/plugin-react';
 import { resolve } from 'node:path';
+import magicalSvg from 'vite-plugin-magical-svg';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), magicalSvg({ target: 'react' })],
 	resolve: {
 		alias: {
 			'@': resolve(__dirname, './src'),
