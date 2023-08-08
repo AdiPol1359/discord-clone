52e64230bea411f2f0739335919a33f8a9ebc41a
adipol1359 <aditv1337@gmail.com>
2023-08-08 19:59:59 +0200

    feat(guilds): add default server name in create guild form

--- src/guilds/components/CreateGuildModal/CreateGuildScene/CreateGuildForm/useCreateGuildForm.ts
+++ src/guilds/components/CreateGuildModal/CreateGuildScene/CreateGuildForm/useCreateGuildForm.ts
@@ -1,8 +1,14 @@
+import { useSessionContext } from '@/auth/providers/SessionProvider';
 import { createGuildFormSchema } from '@/guilds/guilds.schemas';
 import { useZodForm } from '@/hooks/useZodForm';
 
 export const useCreateGuildForm = () => {
-	const { handleSubmit, ...rest } = useZodForm(createGuildFormSchema);
+	const { session } = useSessionContext();
+	const { handleSubmit, ...rest } = useZodForm(createGuildFormSchema, {
+		defaultValues: {
+			...(session && { name: `${session.profile.username}'s server` }),
+		},
+	});
 
 	const handleFormSubmit = handleSubmit(() => {
 		console.log('create...');
