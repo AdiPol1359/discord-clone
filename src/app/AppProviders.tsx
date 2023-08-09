e026d54c0005b599016556f33a27797a14510b89
Adrian Polak <aditv1337@gmail.com>
2023-08-09 10:16:45 +0200

    feat: add tanstack query to project (#34)

--- src/app/AppProviders.tsx
+++ src/app/AppProviders.tsx
@@ -1,5 +1,8 @@
 'use client';
 
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 import { SessionProvider } from '@/auth/providers/SessionProvider';
 import { UIProvider } from '@/ui/providers/UIProvider';
 
@@ -9,8 +12,15 @@ type AppProvidersProps = Readonly<{
 	children: ReactNode;
 }>;
 
+const queryClient = new QueryClient({
+	defaultOptions: { queries: { refetchOnWindowFocus: false } },
+});
+
 export const AppProviders = ({ children }: AppProvidersProps) => (
-	<SessionProvider>
-		<UIProvider>{children}</UIProvider>
-	</SessionProvider>
+	<QueryClientProvider client={queryClient}>
+		<SessionProvider>
+			<UIProvider>{children}</UIProvider>
+		</SessionProvider>
+		<ReactQueryDevtools />
+	</QueryClientProvider>
 );
